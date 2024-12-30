31f0b07c78ceb8edd25451407f4576a604abf5f7
Wian Gertenbach <67628074+WianGertenbach@users.noreply.github.com>
2024-12-30 18:15:43 +0200

    Added console messages for testing purposes

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-function checkType(x,y){
-
-    let intX;
-    let intY; 
-    if (typeof x !== 'number' ||typeof y !== 'number'){
-        intX = parseInt(x);
-        intY = parseInt(y);
-    } else {
-        intX = x;
-        intY = y;
-    }
-
-    return {
-        x: intX,
-        y: intY,
-    }
-}
 
 const add = function(x,y) {
     const result = checkType(x,y);
@@ -43,25 +26,28 @@ const divide = function(x,y) {
     //return arr.reduce((sum, current) => sum/current,1);
 };
 
-function operate(x,y,operator) {
+function math(x,y,operator) {
     return operator(x,y);
 }
 
 let userInput1 = '';
 let temp = '';
 let userInput2 = '';
+let currentOperator;
 
 const digits = document.querySelectorAll(".num");
 const operationScreen = document.querySelector('.operation');
 const resultScreen = document.querySelector('.result');
 const operator = document.querySelectorAll('.operator');
+const equals = document.querySelector('.equals');
 
 digits.forEach(button => {
     button.addEventListener('click', ()=> {
         const number = document.createElement('p');
         number.textContent = button.textContent;
         operationScreen.appendChild(number);
-        userInput1 += button.textContent; 
+        userInput1 += button.textContent;
+        console.log("UserInput1 = "+userInput1);
     });
 });
 
@@ -69,9 +55,35 @@ digits.forEach(button => {
 operator.forEach(button => {
     button.addEventListener('click', ()=> {
         temp = userInput1;
-        userInput1 = ''; 
-        currentOperation = add;  
+        console.log("temp = "+temp);
+        userInput1 = '';
+        console.log("UserInput1 = "+userInput1);
+        currentOperator = multiply;
     })
 });
 
+equals.addEventListener('click', ()=> {
+    result = math(temp,userInput1,currentOperator);
+    console.log(`Result: temp (${temp}) x userInput1 (${userInput1}) = ${result}))`);
+    userInput1 = result;
+    console.log("UserInput1 = "+userInput1);
+})
+
+function checkType(x,y){
+
+    let intX;
+    let intY; 
+    if (typeof x !== 'number' || typeof y !== 'number'){
+        intX = parseInt(x);
+        intY = parseInt(y);
+    } else {
+        intX = x;
+        intY = y;
+    }
+
+    return {
+        x: intX,
+        y: intY,
+    };
+};
 
