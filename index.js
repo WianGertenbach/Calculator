eccbe3f52c65cd177fa099861be7f6df7e9b4a87
Wian Gertenbach <67628074+WianGertenbach@users.noreply.github.com>
2024-12-30 16:37:53 +0200

    Added Functions to Detect Clicks
    Code can detect clicks on buttons and add it to the display.

--- index.js
+++ index.js
@@ -17,3 +17,27 @@ const divide = function(x,y) {
     return x/y;
     //return arr.reduce((sum, current) => sum/current,1);
 };
+
+const numButtonClicked = document.querySelectorAll('.num');
+const operatorButtonClicked = document.querySelectorAll('.operator');
+const screenOperation = document.querySelector('.operation');
+const screenResult = document.querySelector('.result');
+let firstVal = '';
+let secondVal = '';
+
+numButtonClicked.forEach((button) => {
+    button.addEventListener('click', () =>{
+        const num = document.createElement('p');
+        num.textContent = button.textContent;
+        screenOperation.appendChild(num);
+        firstVal += button.textContent;
+    })
+})
+
+operatorButtonClicked.forEach((button) => {
+    button.addEventListener('click', () =>{
+        const operator = document.createElement('p');
+        operator.textContent = button.textContent;
+        screenOperation.appendChild(operator);
+    })
+})
