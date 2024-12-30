44789cccc9ec75dc4cbc1c78fbed94bab344b6cb
Wian Gertenbach <67628074+WianGertenbach@users.noreply.github.com>
2024-12-30 16:07:55 +0200

    Added Mathematical Functions
    Added the functions to evaluate expressions.

---
+++ index.js
@@ -0,0 +1,19 @@
+const add = function(x,y) {
+    return x+y;
+    //return arr.reduce((sum, current) => sum+current,0);
+};
+
+const subtract = function(x,y) {
+    return x-y;
+    //return arr.reduce((sum, current) => sum-current);
+  };
+
+const multiply = function(x,y) {
+    return x*y;
+    //return arr.reduce((sum, current) => sum*current,1);
+};
+
+const divide = function(x,y) {
+    return x/y;
+    //return arr.reduce((sum, current) => sum/current,1);
+};
